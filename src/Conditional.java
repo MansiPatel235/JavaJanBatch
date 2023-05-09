79075ad25de40d54988ce92a2f9d01f3c28cc962
MansiPatel235 <126035111+MansiPatel235@users.noreply.github.com>
2023-05-08 21:55:42 -0400

    Updated by Mansi

--- src/Conditional.java
+++ src/Conditional.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Conditional {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Please Enter Value for a");
+        System.out.println("Please enter Value for a");
         int a = sc.nextInt();
         System.out.println("Please Enter Value for b");
         int b= sc.nextInt();
